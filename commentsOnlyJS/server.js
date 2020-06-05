488eca9abe73e5719d968abc62b796bb14e02b06
Mike <maquinde@outlook.com>
2020-06-05 10:17:17 -0700

    added basic server setup from notes

--- commentsOnlyJS/server.js
+++ commentsOnlyJS/server.js
@@ -11,15 +11,29 @@ const app = express();
 /* Dependencies */
 /* Middleware*/
 
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
 //Here we are configuring express to use body-parser as middle-ware.
 // Cors for cross origin allowance
 
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
+app.use(cors());
 
 // Initialize the main project folder
 
+app.use(express.static('website'));
+
 // Spin up the server
 // Callback to debug
+
+const port = 8080;
+
+const server = app.listen(port, ()=>{
+    console.log(`listening on port: ${port}`);
+});
 
 // Initialize all route with a callback function
 
