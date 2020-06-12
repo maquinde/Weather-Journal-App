7022a23e2f23be721308337f267b4ed40f1c240b
Mike <maquinde@outlook.com>
2020-06-11 21:45:26 -0700

    starting from scratch

--- server.js
+++ server.js
@@ -1,55 +1,19 @@
 // Setup empty JS object to act as endpoint for all routes
-const projectData = {};
+projectData = {};
 
-
-// Express to run server and routes
-const express = require('express');
-
+// Require Express to run server and routes
 
 // Start up an instance of app
-const app = express();
 
-
-/* Dependencies */
 /* Middleware*/
-
 //Here we are configuring express to use body-parser as middle-ware.
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-
 // Cors for cross origin allowance
-const cors = require('cors');
-app.use(cors());
-
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
-
-// Spin up the server
-const port = 8080;
-const server = app.listen(port, ()=>{
-    console.log(`listening on port: ${port}`);
-});
-
-
-// Initialize all route with a callback function
-// Callback function to complete GET '/all'
-app.get('/all', (req, res) => {
-    res.send(projectData);
-});
-
-
-// Post Route
-const data = [];
-app.post('/all', weatherInfo);  
-
-function weatherInfo(req, res){
-    projectData.date = req.body.date;
-    projectData.temp = req.body.temp;
-    projectData.content = req.body.content;
-    res.send(projectData);
-}
+// Setup Server
--- website/app.js
+++ website/app.js
@@ -1,35 +1,5 @@
-// Personal API Key for OpenWeatherMap API
-const url = "https://api.openweathermap.org/data/2.5/weather?";
-const key = "edf4166a8c76354d02d7cebedd3be30d";
-const urlBuild = `${url}zip=${zipCode}&appid=${key}`;
+/* Global Variables */
 
-
-//Get the date
+// Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
-
-
-// Event listener to add function to existing HTML DOM element
-const button = document.getElementById('generate');
-button.addEventListener('click', execute);
-
-
-/* Function called by event listener */
-function execute(event){
-    event.preventDefault();
-    const zipCode = document.getElementById('zip').value;
-    const feels = document.getElementById('feelings').value;
-}
-
-
-/* Function to GET Web API Data*/
-
-
-
-
-/* Function to POST data */
-
-
-
-
-/* Function to GET Project Data */
+let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
