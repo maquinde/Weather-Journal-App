350024d2d037e5a0ede013567de6408ad97d4970
Mike <maquinde@outlook.com>
2020-06-13 22:26:26 -0700

    completed serverside

--- server.js
+++ server.js
@@ -2,18 +2,46 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
+
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
-
 // Setup Server
+const port = 8080;
+const server = app.listen(port, listening);
+
+function listening(){
+    console.log(`Server on localhost:${port}`);
+}
+
+// Initialize '/all' route with a callback function
+app.get('/all', sendData);
+
+// Callback function to complete GET '/all'
+function sendData(req, res){
+    res.send(projectData);
+}
+
+// Post Route
+app.post('/', (req, res) => {
+    projectData.temp = req.body.main.temp;
+    projectData.date = req.body.date;
+    projectData.feeling = req.body.feeling;
+    console.log('POST Received!');
+    res.end();
+});
--- website/app.js
+++ website/app.js
@@ -1,5 +1,50 @@
 /* Global Variables */
+// Personal API Key for OpenWeatherMap API
+const url = 'http://api.openweathermap.org/data/2.5/weather?zip=';
+const apiKey = '&appid=edf4166a8c76354d02d7cebedd3be30d';
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+
+
+/* Function called by event listener */
+const getData = async () => {
+    const zip = document.querySelector('#zip').value;
+    const feeling = document.querySelector('#feelings').value;
+    const weatherData = await fetch(`${url}${zip}${apiKey}`);
+
+    try {
+        const weatherData_json = await weatherData.json();
+        weatherData_json.feeling = feeling;
+        weatherData_json.date = newDate;
+
+
+
+    } catch(error){
+        console.log('error', error);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+// Event listener to add function to existing HTML DOM element
+document.querySelector('#generate').addEventListener('click', getData);
+
+
+//========================COMMENT ONLY====================
+
+/* Function to GET Web API Data*/
+
+/* Function to POST data */
+
+/* Function to GET Project Data */
